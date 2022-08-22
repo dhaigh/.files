94e72011457691011ced0aa87d12f4d05e994479
Declan Haigh <declan@declanhaigh.com>
2022-08-22 13:58:46 +1000

    Add alacritty config and nvim dir

--- install.sh
+++ install.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
 # make symlinks for dotfiles
-[ ! -e ~/.config/nvim ] && ln -s ~/.files/.config/nvim ~/.config/nvim
-[ ! -e ~/.tmux.conf   ] && ln -s ~/.files/.tmux.conf   ~/.tmux.conf
-[ ! -e ~/.vim         ] && ln -s ~/.files/.vim         ~/.vim
-[ ! -e ~/.vimrc       ] && ln -s ~/.files/.vimrc       ~/.vimrc
-[ ! -e ~/.zshrc       ] && ln -s ~/.files/.zshrc       ~/.zshrc
+mkdir -pv ~/.config/nvim
+[ ! -e ~/.config/nvim   ] && ln -s ~/.files/.config/nvim         ~/.config/nvim
+[ ! -e ~/.tmux.conf     ] && ln -s ~/.files/.tmux.conf           ~/.tmux.conf
+[ ! -e ~/.vim           ] && ln -s ~/.files/.vim                 ~/.vim
+[ ! -e ~/.vimrc         ] && ln -s ~/.files/.vimrc               ~/.vimrc
+[ ! -e ~/.zshrc         ] && ln -s ~/.files/.zshrc               ~/.zshrc
+[ ! -e ~/.alacritty.yml ] && ln -s ~/.files/.alacritty.yml       ~/.alacritty.yml
 
 # install tmux plugins
 [ ! -e ~/.tmux/plugins/tpm ] &&
